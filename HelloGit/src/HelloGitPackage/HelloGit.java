201c6a90ceb6e5f4136bf7eac6e953d7f7045060
Delyan Delev <delyan.delev@estafet.com>
2018-05-09 13:17:43 +0300

    second video

--- HelloGit/src/HelloGitPackage/HelloGit.java
+++ HelloGit/src/HelloGitPackage/HelloGit.java
@@ -3,8 +3,10 @@ package HelloGitPackage;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!!!");
-		//greetings
+		String[] names = {"Bilbo","Frodo","Aragorn","Legolas","gandalf"};
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Hello , %s/n",names[i]);
+		}
 	}
 
 }
